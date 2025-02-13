2cdfb8ba68fbe30eb2654adc62056a98e228d046
Y0zh1kB0kut0 <shekurov3003@gmail.com>
2025-02-14 00:01:00 +0300

    1

--- MyGame/Assets/Scripts/DestroyTree.cs
+++ MyGame/Assets/Scripts/DestroyTree.cs
@@ -1,27 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DestroyTree : MonoBehaviour
 {
     public TMP_Text text;
-
-    //private void OnCollisionStay(Collision collision)
-    //{
-    //    if (collision.gameObject.tag == "Tree")
-    //    {
-    //        text.text = "Нажмите ЛКМ, чтобы начать рубить";
-    //    }
-    //}
-
-    //private void OnCollisionExit(Collision collision)
-    //{
-    //    if (collision.gameObject.tag == "Tree")
-    //    {
-    //        text.text = "";
-    //    }
-    //}
+    public Image imageHP;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,6 +22,20 @@ public class DestroyTree : MonoBehaviour
         if (other.gameObject.tag == "Tree")
         {
             text.text = "";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Damaging();
+    }
+
+    public void Damaging()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Я здесь");
+            imageHP.fillAmount -= 0.1f;
         }
     }
 }
