0aaffb9fa9085abe6b95f6e425aacbffa42f464a
Y0zh1kB0kut0 <shekurov3003@gmail.com>
2025-02-15 12:22:23 +0300

    update 2 15.02.25

--- MyGame/Assets/Scripts/DestroyTree.cs
+++ MyGame/Assets/Scripts/DestroyTree.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor;
 
 public class DestroyTree : MonoBehaviour
 {
     public TMP_Text text;
-    public Image imageHP;
+
+    private Image imageHP;
+
+    public bool onTrigger;
+
+    private GameObject tree;
+    public GameObject prefabWood;
+
+    private Transform canvas;
+    private Transform image;
+
+
+    private void Update()
+    {
+        if (onTrigger)
+        {
+            Damaging();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Tree")
         {
             text.text = "Нажмите ЛКМ, чтобы начать рубить";
+            onTrigger = true;
+            tree = other.gameObject;
+            canvas = tree.transform.GetChild(1);
+            image = canvas.transform.GetChild(0);
+            imageHP = image.GetComponent<Image>();
         }
     }
 
@@ -22,20 +46,42 @@ public class DestroyTree : MonoBehaviour
         if (other.gameObject.tag == "Tree")
         {
             text.text = "";
+            onTrigger = false;
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        Damaging();
-    }
 
     public void Damaging()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Я здесь");
             imageHP.fillAmount -= 0.1f;
         }
+        if (imageHP.fillAmount <= 0)
+        {
+            StartCoroutine("CreateWood");
+            text.text = "";
+            Destroy(tree);
+            onTrigger = false;
+        }
+    }
+
+    IEnumerator CreateWood() 
+    {
+        float posX = tree.transform.position.x;
+        float posZ = tree.transform.position.z;
+        List<GameObject> woods = new List<GameObject>();
+        Rigidbody _rb;
+        for (int i = 0; i < 3; i++)
+        {
+            woods.Add(Instantiate(prefabWood, new Vector3(Random.Range(posX - 1f, posX + 1f), .3f, Random.Range(posZ - 1f, posZ + 1f)), Quaternion.identity));
+        }
+        yield return new WaitForSeconds(.15f);
+        foreach (GameObject wood in woods)
+        {
+            _rb = wood.GetComponent<Rigidbody>();
+            _rb.isKinematic = true;
+        }
+
     }
 }
---
+++ MyGame/Assets/Scripts/TakeWood.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TakeWood : MonoBehaviour
+{
+    public TMP_Text text;
+    public TMP_Text countWoodText;
+
+    private int countWood;
+
+    private bool onCollision;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wood")
+        {
+            text.text = "Нажмите К, чтобы подобрать дерево";
+            onCollision = true;
+        } 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.text = "";
+        onCollision = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Take(other);
+    }
+
+    void Take(Collider other)
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            countWood++;
+            countWoodText.text = "Дерева: " + Convert.ToString(countWood);
+            text.text = "";
+            Destroy(other.gameObject);
+        }
+    }
+}
